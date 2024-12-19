7e0dcbcd1cee4331fbc369029f0183fa45a9c460
openglobus <github-actions[bot]@users.noreply.github.com>
2024-12-19 07:42:07 +0000

    Update from https://github.com/openglobus/og-homepage/commit/e3d7e66f647ceb2604a94c86bace6e3153298992

--- storybook/assets/Globe.stories-Cd-4kiby.js
+++
@@ -1,9 +0,0 @@
-import{G as p}from"./Globe-CrL9GLUm.js";import"./index-RYns6xqu.js";const d={component:p,title:"Components/Scenes/Globe"},e={args:{atmosphereEnabled:!1}},r={args:{atmosphereEnabled:!0}};var a,s,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    atmosphereEnabled: false
-  }
-}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    atmosphereEnabled: true
-  }
-}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const u=["Default","AtmosphereEnabled"];export{r as AtmosphereEnabled,e as Default,u as __namedExportsOrder,d as default};
--- storybook/assets/preview-CADA8DHW.js
+++
@@ -1,1 +0,0 @@
-const o={tags:["autodocs"],parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{o as default};
