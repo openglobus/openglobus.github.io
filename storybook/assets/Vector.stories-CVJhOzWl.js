c9a3295e3a5596a0ea7cdb9c05bafbaa07d22eb6
openglobus <github-actions[bot]@users.noreply.github.com>
2025-06-25 07:43:48 +0000

    Update from https://github.com/openglobus/openglobus-react/commit/7254d935e57aaa9ba79fd1ad1e571f98d3c8e960

---
+++ storybook/assets/Vector.stories-CVJhOzWl.js
@@ -0,0 +1,24 @@
+import{V as s,E as n}from"./Entity-Co4qv4ZC.js";import{Default as m}from"./Layer.stories-CC2TsQ1E.js";import{B as p}from"./Billboard-8TYe2nvQ.js";import{R as e}from"./index-D4lIrffr.js";import{a as c,G as i}from"./Globe-DQK9_liM.js";import"./v4-CtRu48qb.js";const E={title:"Components/Layer/Vector",component:s},t={parameters:{actions:{depth:1,maxDepth:1}},argTypes:{...m.argTypes},args:{name:"test",opacity:1},render:l=>e.createElement(c,null,e.createElement(i,null,e.createElement(s,{...l},e.createElement(n,{name:"Custom Entity",lon:0,lat:0,alt:0},e.createElement(p,{size:[96,96],color:"#ff5959",src:"https://sandbox.openglobus.org/examples/billboards/carrot.png"})))))};var r,o,a;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  parameters: {
+    actions: {
+      depth: 1,
+      maxDepth: 1
+    }
+  },
+  argTypes: {
+    ...LayerDefault.argTypes
+  },
+  args: {
+    name: 'test',
+    opacity: 1
+  },
+  render: (args: VectorProps) => <GlobeContextProvider>
+        <Globe>
+            <Vector {...args}>
+                <Entity name="Custom Entity" lon={0} lat={0} alt={0}>
+                    <Billboard size={[96, 96]} color={"#ff5959"} src={'https://sandbox.openglobus.org/examples/billboards/carrot.png'} />
+                </Entity>
+            </Vector>
+        </Globe>
+    </GlobeContextProvider>
+}`,...(a=(o=t.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const x=["Default"];export{t as Default,x as __namedExportsOrder,E as default};
